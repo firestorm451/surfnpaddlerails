217ec4621cd8702023c339fa5d7ea5ff40707cf3
Matthew Zwick <moxie@Matthews-MacBook-Pro.local>
2016-03-28 16:45:31 -0400

    Working model

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -3,10 +3,11 @@ class WebsiteController < ApplicationController
   def index
     @posts = Post.all
     @post = @posts.max_by{|p| p.created_at}
+    @posts = @posts - [@post]
   end
 
   def show
-    @posts = Post.all
+    @posts = Post.all.reject{|post| post.id == params[:id].to_i}
     @post = Post.find(params[:id].to_i)
     render 'index'
   end
